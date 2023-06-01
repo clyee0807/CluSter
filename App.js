b9034ea272319acb1c5bc8ecad3b87df9ddbff51
clyee <happying910807@gmail.com>
2023-06-02 04:46:11 +0800

    entry cannot tap

--- App.js
+++ App.js
@@ -1,34 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { useFonts, FugazOne_400Regular } from '@expo-google-fonts/fugaz-one';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+import { Login } from './Login.js';
+
+function App ({navigation}) {
+  let [fontsLoaded] = useFonts({
+    FugazOne_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>CluSter</Text>
-      <Text style={styles.subtitle}>Tap to Start</Text>
-      <Text style={styles.slogan}>Transcending the Universe, Our Encounter Awaits.</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <LinearGradient colors= {['#0F1A37', '#2C4073', '#07070F']} style={styles.lineargradient}>
+    <ImageBackground source={require('./assets/Home.png')} style={styles.backgroundImage}>
+      {/* <TouchableOpacity activeOpacity={0.8} style={styles.touchContainer} onPress={()=>navigation.navigate('Login')}> */}
+        <View style={styles.container}>  
+          <Text style={styles.title}>CluSter</Text>
+          <Text style={styles.subtitle}>Tap to Start</Text>
+          <Text style={styles.slogan}>Transcending the Universe, Our Encounter Awaits.</Text>
+          <StatusBar style="auto" />
+        </View> 
+      {/* </TouchableOpacity> */}
+    </ImageBackground>
+    //</LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#2C4073',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  // lineargradient: {
+  //   flex: 1,
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'contain'
+  },
   title: {
-    // fontFamily: 'Fugaz One',
+    fontFamily: 'FugazOne_400Regular',
     color: '#fff',
-    fontSize: 60,
-    marginBottom: 100
+    fontSize: 64,
+    marginTop: 260
   },
   subtitle: {
-    color: '#fff'
+    color: '#fff',
+    fontFamily: 'FugazOne_400Regular',
+    fontSize: 24,
+    marginTop: 90
+
   },
   slogan: {
-    color: '#fff'
+    color: '#fff',
+    fontFamily: 'FugazOne_400Regular',
+    width: '50%',
+    textAlign: 'center',
+    marginTop: 60,
+    marginBottom: 60
+
   }
 });
+
+// export {App, createAppContainer};
+export default App;
---
+++ Login.js
@@ -0,0 +1,11 @@
+import { ImageBackground, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+function Login(){
+    return (
+        <View>
+            <Text>LOGIN</Text>
+        </View>
+    );
+}
+
+export default Login;
