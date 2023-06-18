6a801d7f02448e34332494af57fded95e2267e84
clyee <happying910807@gmail.com>
2023-06-18 10:55:03 +0800

    selectList, textBar

--- screens/createEvents_Screens/Main.js
+++ screens/createEvents_Screens/Main.js
@@ -1,9 +1,13 @@
 // createEvent - weekday/date 
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { SpaceGrotesk_400Regular } from '@expo-google-fonts/space-grotesk';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import Buttons from '../../components/buttons.js';
 import {Calendar, LocaleConfig} from 'react-native-calendars';
+import BottomBar from '../../components/bottomBar';
+import TextBar from '../../components/textBar.js';
+// import ModalDropdown from 'react-native-modal-dropdown';
+// import DropDownPicker from 'react-native-dropdown-picker';
+import { SelectList } from 'react-native-dropdown-select-list'
 
 const Exampledata = [
     {
@@ -35,7 +39,7 @@ const Exampledata = [
 
 
 
-export default function CreateEvent() {
+export default function CreateEvent({navigation}) {
 	const [selectedDate, setSelected] = useState([]);
 	const [mode, setMode] = useState('Specific Dates'); 
 
@@ -44,7 +48,7 @@ export default function CreateEvent() {
 		// console.log(mode);
 	};
 	const handleSetSelected = (day) => {
-		if(selectedDate.includes(day.dateString)){
+		if(selectedDate.includes(day.dateString)){  // 選過的再點要把她刪掉
 			setSelected((prevSelected) => {
 				let filtSelected = prevSelected.filter(item => item !== day.dateString);
 				return [...filtSelected];
@@ -62,6 +66,14 @@ export default function CreateEvent() {
 		markdates[date] = {selected: true, selectedDayBackgroundColor: '#809BBF'};
 	});
 
+	const submitEventNameHandler = (text) => {  
+		// console.log(todos);
+	
+	}
+
+	// dropDown
+	const [dropSelected, setSdropelected] = React.useState("");
+
 	return (
 		<View style={styles.container}>
 			<View style={styles.modeButton}>
@@ -72,12 +84,13 @@ export default function CreateEvent() {
 				(<View>
 					<Text>Days of the Week</Text>
 				</View>) : 
-				(<View>
+				(<ScrollView style={styles.topSection}>
 					<Calendar
-					  	// markingType={'period'}
 						markedDates={markdates}
 						style={{
+							// width: 300,
 							marginHorizontal: 35,
+							marginBottom: 15,
 							borderWidth: 1,
 							borderRadius: 12,
 							borderColor: '#809BBF',
@@ -87,26 +100,78 @@ export default function CreateEvent() {
 							selectedDayBackgroundColor: '#809BBF',
 							selectedDayTextColor: '#ffffff',
 							todayTextColor: '#FF63A5',
+							textDayFontSize: 14,
+							textMonthFontSize: 16,
+							arrowColor: '#1C1243',
 						}}
 						onDayPress={day => {
 							handleSetSelected(day);
-
+							
 						}}
+						/>
+					<Text style={styles.settingText}>Event Name: </Text>
+					<TextBar placeholder="Type a name..." submitHandler={submitEventNameHandler}/>
+					<Text style={styles.settingText}>Team Members: </Text>
+					{/* 大頭照是要怎麼搞== */}
+					<Text style={styles.settingText}>Setting: </Text>
+					<Text style={styles.subsettingText}>Minimum Time Unit: </Text>
+					<SelectList 
+						setSelected={(val) => setSdropelected(val)} 
+						data={[{key:'1', value:'1 hour'},
+							  {key:'2', value:'2 hour'}]} 
+						save="value"
+						search={false}
+						boxStyles={{marginHorizontal:32,
+									marginTop: 10,
+									marginBottom: 10,
+									fontFamily: 'SpaceGrotesk_400Regular',
+									borderColor: '#809BBF',
+									borderRadius: 16, }}
+						dropdownStyles={{marginHorizontal:32,
+									borderColor: '#809BBF',
+									fontFamily: 'SpaceGrotesk_400Regular',
+									marginTop: 2,
+									marginBottom: 10,
+									}}
+						defaultOption={{ key:'1', value:'1 hour' }}
 					/>
-				</View>)}
+					<Text style={styles.subsettingText}>Interval: </Text>
 
+				</ScrollView>
+				)}
+
+		<BottomBar navigation={navigation}/>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
-
+		backgroundColor: "#fff",
+		flex: 1,
+	},
+	topSection: {
+		// backgroundColor: 'pink',
+		marginBottom: 70,
+		// flex: 1,
 	},
 	modeButton: {
-		margin: 15,
+		marginVertical: 15,
+		marginHorizontal: 10,
 		flexDirection: 'row',
 		justifyContent: 'space-around', 
 		alignItems: 'flex-start',
+	},
+	settingText: {
+		marginLeft: 35,
+		marginBottom: 10,
+		fontFamily: 'SpaceGrotesk_400Regular',
+		fontSize: 16,
+	},
+	subsettingText: {
+		marginLeft: 38,
+
+		fontFamily: 'SpaceGrotesk_400Regular',
+		fontSize: 12,
 	},
 });
