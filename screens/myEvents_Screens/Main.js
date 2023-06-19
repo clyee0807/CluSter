27c67bc5b5b09665d769f69a817ff9bec9435d99
clyee <happying910807@gmail.com>
2023-06-19 21:16:57 +0800

    my event filter

--- screens/myEvents_Screens/Main.js
+++ screens/myEvents_Screens/Main.js
@@ -9,81 +9,140 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 export default function MyEvent({navigation}) {
 
   const [events, setEvent] = useState([
-    { id: '1',
-      event_name: 'MyEvent1',
-      dates: ['2023/5/20', '2023/5/21', '2023/5/22'], // array of string, storing dates
-      host: 'NOT911', // string, storing user_id of 'user'
-      members: ['John', 'Peggy', 'Daphne'], // array of string, storing user_id of 'user'
-      interval: ['9:00', '10:00', '11:00'], // array of string, storing times
-      description: 'This is description.', // string
-      deadline: '2023-05-04 22:00', // string, storing date and time
-      status: 'In progress',
-      event_code: '809BBF', // string
-      // Use (date, time) to access the time block.
-      // For example, (0, 0) stands for 2023/5/20 9:00.
-      available_member: [
-          [['111111', '222222', '333333'], ['111111', '222222'], []],
-          [['333333'], ['333333'], []],
-          [[], [], []],
-      ], // 3-D array of string, storing user_id of 'user'
-      confirmTime: 'na' // string, storing date and time
-    },
-    { id: '2',
-      event_name: 'MyEvent2',
-      dates: ['2023/5/20', '2023/5/21', '2023/5/22'], 
-      host: 'NOT911', 
-      members: ['Domingo', 'xincc'], 
-      interval: ['9:00', '10:00', '11:00'], 
-      description: 'This is description.', 
-      deadline: 'na', 
-      status: 'Settled',
-      event_code: '809BBF', 
-      available_member: [
-          [['111111', '222222', '333333'], ['111111', '222222'], []],
-          [['333333'], ['333333'], []],
-          [[], [], []],
-      ],
-      confirmTime: 'na' 
-    },
-    { id: '3',
-      event_name: 'MyEvent2',
-      dates: ['2023/5/20', '2023/5/21', '2023/5/22'], 
-      host: 'NOT911', 
-      members: ['111111', '222222', '333333'], 
-      interval: ['9:00', '10:00', '11:00'], 
-      description: 'This is description.', 
-      deadline: '2023-05-05  19:00', 
-      status: 'In progress',
-      event_code: '809BBF', 
-      available_member: [
-          [['111111', '222222', '333333'], ['111111', '222222'], []],
-          [['333333'], ['333333'], []],
-          [[], [], []],
-      ],
-      confirmTime: 'na' 
-    },
-    { id: '4',
-      event_name: 'MyEvent2',
-      dates: ['2023/5/20', '2023/5/21', '2023/5/22'], 
-      host: 'NOT911', 
-      members: ['111111', '222222', '333333'], 
-      interval: ['9:00', '10:00', '11:00'], 
-      description: 'This is description.', 
-      status: 'Settled',
-      deadline: 'na', 
-      event_code: '809BBF', 
-      available_member: [
-          [['111111', '222222', '333333'], ['111111', '222222'], []],
-          [['333333'], ['333333'], []],
-          [[], [], []],
-      ],
-      confirmTime: 'na' 
-    },
+    {
+		id: '1',
+		event_name: 'MyEvent1',
+		dates: ['2023/7/20', '2023/7/21', '2023/7/22'], // array of string, storing dates
+		host: 'Domingo', // string, storing username of 'user'
+		members: ['Guavaaa', 'Jason', 'Tony', 'Bear'], // array of string, storing username of 'user'
+		interval: ['9:00', '10:00', '11:00'], // array of string, storing times
+		description: 'This is the description.', // string
+		deadline: '2023/7/4 22:00', // string, storing date and time
+		status: 'In progress', // string, indicating the event is in progress or settled
+		event_code: '809BBF', // string
+		// Use (date, time) to access the time block.
+		// For example, (0, 0) stands for 2023/5/20 9:00.
+		available_member: [
+		  [['Domingo', 'Guavaaa', 'Jason', 'Tony', 'Bear'], ['Domingo', 'Guavaaa', 'Jason', 'Tony'], ['Domingo', 'Guavaaa', 'Jason', 'Tony']],
+		  [['Guavaaa', 'Jason', 'Bear'], ['Guavaaa', 'Tony'], ['Guavaaa', 'Tony']],
+		  [['Guavaaa', 'Jason', 'Bear'], ['Guavaaa'], []],
+		], // 3-D array of string, storing username of 'user'
+		topTimeBlock: [
+		  [[0, 0]],
+		  [[0, 1], [0, 2]],
+		  [[1, 0], [2, 0]]
+		], // 3-D array of number, storimg correpsonding time block
+		confirmTime: 'na' // string, storing date and time
+	  },
+	  { 
+		id: '2',
+		event_name: 'FOOBAR',
+		dates: ['2023/7/1', '2023/7/2'],
+		host: 'Guavaaa',
+		members: ['Domingo', 'Jason', 'Tony'],
+		interval: ['18:00', '19:00', '20:00', '21:00'],
+		description: 'FOOBAR',
+		deadline: '2023/5/1 23:59',
+		status: 'Settled',
+		event_code: '1A2B3C',
+		available_member: [
+		  [['Domingo', 'Guavaaa'], ['Domingo', 'Guavaaa'], ['Domingo', 'Guavaaa', 'Jason'], ['Domingo', 'Guavaaa', 'Jason', 'Tony']],
+		  [[], ['Tony'], ['Guavaaa', 'Jason', 'Tony'], ['Guavaaa', 'Jason', 'Tony']],
+		],
+		topTimeBlock: [
+		  [[0, 3]],
+		  [[0, 2], [1, 2], [1, 3]],
+		  [[0, 1], [0, 1]]
+		],
+		confirmTime: '2023/7/1 18:00'
+	  },
+	  { 
+		id: '3',
+		event_name: 'Emotional Damage',
+		dates: ['2023/6/26', '2023/6/27', '2023/6/28', '2023/6/29', '2023/6/30'],
+		host: 'Domingo',
+		members: ['Guavaaa', 'Jason', 'Tony', 'Bear'],
+		interval: ['9:00', '10:00'],
+		description: 'I will send you to Jesus.',
+		deadline: '2023/6/20  23:59',
+		status: 'In progress',
+		event_code: 'STEVEN', 
+		available_member: [
+		  [['Domingo', 'Guavaaa', 'Jason', 'Bear'], ['Domingo', 'Guavaaa', 'Tony']],
+		  [['Domingo', 'Guavaaa', 'Jason', 'Bear'], ['Domingo', 'Guavaaa', 'Tony']],
+		  [['Domingo', 'Guavaaa', 'Jason'], ['Domingo', 'Guavaaa', 'Tony']],
+		  [['Domingo', 'Jason'], ['Domingo', 'Tony']],
+		  [['Domingo', 'Jason'], ['Domingo', 'Tony']]
+		],
+		topTimeBlock: [
+		  [[0, 0], [1, 0]],
+		  [[0, 1], [1, 1], [2, 0], [2, 1]],
+		  [[3, 0], [3, 1], [4, 0], [4, 1]]
+		],
+		confirmTime: 'na'
+	  },
+	  {
+		id: '4',
+		event_name: 'heyyeyaaeyaaaeyaeyaa',
+		dates: ['2023/10/1', '2023/10/2', '2023/10/8', '2023/10/9'],
+		host: 'Jason',
+		members: ['Domingo', 'Guavaaa', 'Tony'],
+		interval: ['9:00', '11:00', '13:00', '15:00'],
+		description: 'Whats up.',
+		deadline: '2023/9/1  23:59',
+		status: 'In progress',
+		event_code: '0H0E2E', 
+		available_member: [
+		  [['Guavaaa'], ['Guavaaa'], ['Guavaaa'], ['Domingo', 'Guavaaa']],
+		  [['Guavaaa'], ['Guavaaa'], ['Guavaaa'], ['Domingo', 'Guavaaa']],
+		  [['Jason'], ['Jason'], ['Jason'], ['Domingo', 'Jason', 'Tony']],
+		  [['Jason'], ['Jason'], ['Jason'], ['Domingo', 'Guavaaa','Jason', 'Tony']],
+		],
+		topTimeBlock: [
+		  [[3, 3]],
+		  [[2, 3]],
+		  [[0, 3], [1, 3]]
+		],
+		confirmTime: 'na'
+	  },
+	  {
+		id: '5',
+		event_name: 'Badminton',
+		dates: ['2023/6/26', '2023/6/27', '2023/6/28'],
+		host: 'Bear',
+		members: ['Domingo', 'Guavaaa', 'Jason'],
+		interval: ['16:00', '17:00', '18:00', '19:00'],
+		description: 'Hit badminton',
+		deadline: '2023/6/21  23:59',
+		status: 'In progress',
+		event_code: '6969SX', 
+		available_member: [
+		  [['Domingo', 'Guavaaa', 'Bear'], ['Domingo', 'Guavaaa', 'Bear'], ['Domingo', 'Guavaaa', 'Bear'], ['Domingo', 'Guavaaa', 'Bear', 'Jason']],
+		  [['Domingo', 'Bear'], ['Domingo', 'Bear'], ['Domingo', 'Bear'], ['Domingo', 'Bear']],
+		  [['Jason'], ['Jason'], ['Jason'], ['Jason']]
+		],
+		topTimeBlock: [
+		  [[0, 3]],
+		  [[0, 0], [0, 1], [0, 2]],
+		  [[1, 0], [1, 1], [1, 2], [1, 3]]
+		],
+		confirmTime: 'na'
+	  }
 	]);
 
-  const submitHandler = (text) => {  
-    // console.log(todos);
-
+	const [filtEvent, setFiltEvent] = useState(events);
+	const submitHandler = (text) => {  
+		if(text === ''){
+			setFiltEvent((prevState) => {
+				return [...events]
+			})
+		} else {
+			setFiltEvent((prevState) => {
+				let filted = events.filter(item => item.event_name.includes(text));
+				return [...filted]
+			})
+		}
+					
 	}
 
 
@@ -100,7 +159,7 @@ export default function MyEvent({navigation}) {
 				<SearchBar submitHandler={submitHandler}/>
 				<View style={styles.list}>
 					<FlatList
-					data={events}
+					data={filtEvent}
 					renderItem={ ({item})=>(
 						<TouchableOpacity onPress={() => navigation.navigate('EventScreen')}>
 						<View style={styles.eventCard}>
